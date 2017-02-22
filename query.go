f1a8afd17c3e3c68d75d66d7ebaefaa43e086159
Foolin <foolin@126.com>
2017-02-22 09:31:10 +0800

    add count function

--- query.go
+++ query.go
@@ -7,14 +7,15 @@ import (
 )
 
 type QueryBuilder struct {
-	db *Dbx
-	sqlTable string
-	sqlWhere string
-	sqlArgs []interface{}
-	sqlSelect string
-	sqlSort string
-	sqlOffset int
-	sqlLimit int
+	db         *Dbx
+	sqlTable   string
+	sqlWhere   string
+	sqlArgs    []interface{}
+	sqlSelect  string
+	sqlGroupBy string
+	sqlOrderBy string
+	sqlOffset  int
+	sqlLimit   int
 }
 
 func NewQuery(db *Dbx, table string) QueryBuilder {
@@ -25,47 +26,50 @@ func NewQuery(db *Dbx, table string) QueryBuilder {
 	}
 }
 
-func (db *Dbx) NewQuery(table string) QueryBuilder{
+func (db *Dbx) NewQuery(table string) QueryBuilder {
 	return NewQuery(db, table)
+}
+
+func (q QueryBuilder) Select(fields ...string) QueryBuilder {
+	q.sqlSelect = strings.Join(fields, ",")
+	return q
 }
 
 func (db *Dbx) Where(table string, where string, args ...interface{}) QueryBuilder {
 	return db.NewQuery(table).Where(where, args...)
 }
 
-func (q QueryBuilder) Where(where string, args ...interface{}) QueryBuilder{
+func (q QueryBuilder) Where(where string, args ...interface{}) QueryBuilder {
 	q.sqlWhere = where
 	q.sqlArgs = args
 	return q
 }
 
-func (q QueryBuilder) Select(fields ...string) QueryBuilder{
-	q.sqlSelect = strings.Join(fields, ",")
+func (q QueryBuilder) GroupBy(fields ...string) QueryBuilder {
+	q.sqlGroupBy = strings.Join(fields, ",")
 	return q
 }
 
-
-func (q QueryBuilder) Sort(fields ...string) QueryBuilder{
-	q.sqlSort = strings.Join(fields, ",")
+func (q QueryBuilder) OrderBy(fields ...string) QueryBuilder {
+	q.sqlOrderBy = strings.Join(fields, ",")
 	return q
 }
 
-
-func (q QueryBuilder) Offset(offset int) QueryBuilder{
+func (q QueryBuilder) Offset(offset int) QueryBuilder {
 	q.sqlOffset = offset
 	return q
 }
 
-func (q QueryBuilder) Limit(limit int) QueryBuilder{
+func (q QueryBuilder) Limit(limit int) QueryBuilder {
 	q.sqlLimit = limit
 	return q
 }
 
-func (q QueryBuilder) Page(page int, pageSize int) QueryBuilder{
-	if page < 1{
+func (q QueryBuilder) Page(page int, pageSize int) QueryBuilder {
+	if page < 1 {
 		page = 1
 	}
-	if pageSize <= 0{
+	if pageSize <= 0 {
 		pageSize = 20
 	}
 	q.sqlOffset = (page - 1) * pageSize
@@ -73,56 +77,66 @@ func (q QueryBuilder) Page(page int, pageSize int) QueryBuilder{
 	return q
 }
 
-func (q QueryBuilder) Sql() string{
-	sql := fmt.Sprintf("SELECT %v FROM %v", q.sqlSelect, q.sqlTable)
-	if q.sqlWhere != ""{
+func (q QueryBuilder) Sql() string {
+	sql := fmt.Sprintf("SELECT %v", q.sqlSelect)
+	sql += " FROM " + q.sqlTable
+	if q.sqlWhere != "" {
 		sql += " WHERE " + q.sqlWhere
 	}
-	if q.sqlSort != ""{
-		sql += " ORDER BY " + q.sqlSort
+	if q.sqlGroupBy != "" {
+		sql += " GROUP BY " + q.sqlGroupBy
 	}
-	if q.sqlLimit > 0{
+	if q.sqlOrderBy != "" {
+		sql += " ORDER BY " + q.sqlOrderBy
+	}
+	if q.sqlLimit > 0 {
 		sql += fmt.Sprintf(" LIMIT %v", q.sqlLimit)
 	}
-	if q.sqlOffset > 0{
+	if q.sqlOffset > 0 {
 		sql += fmt.Sprintf(" OFFSET %v", q.sqlOffset)
 	}
 	return sql
 }
 
-func (q QueryBuilder) Args() []interface{}{
+func (q QueryBuilder) Args() []interface{} {
 	return q.sqlArgs
 }
 
-func (q QueryBuilder) One(dest interface{}) error{
-	if q.sqlLimit <= 0{
+func (q QueryBuilder) One(dest interface{}) error {
+	if q.sqlLimit <= 0 {
 		q.sqlLimit = 1
 	}
 	return q.db.QueryOne(dest, q.Sql(), q.Args()...)
 }
 
-func (q QueryBuilder) All(dest interface{}) error{
+func (q QueryBuilder) All(dest interface{}) error {
 	return q.db.QueryAll(dest, q.Sql(), q.Args()...)
 }
 
-func (q QueryBuilder) Row() *sql.Row{
-	if q.sqlLimit <= 0{
+func (q QueryBuilder) Row() *sql.Row {
+	if q.sqlLimit <= 0 {
 		q.sqlLimit = 1
 	}
 	return q.db.QueryRow(q.Sql(), q.Args()...)
 }
 
-func (q QueryBuilder) Rows() (*sql.Rows, error){
+func (q QueryBuilder) Rows() (*sql.Rows, error) {
 	return q.db.Query(q.Sql(), q.Args()...)
 }
 
-func (q QueryBuilder) MapRow() (MapRow, error){
-	if q.sqlLimit <= 0{
+func (q QueryBuilder) MapRow() (MapRow, error) {
+	if q.sqlLimit <= 0 {
 		q.sqlLimit = 1
 	}
 	return q.db.QueryMapRow(q.Sql(), q.Args()...)
 }
 
-func (q QueryBuilder) MapRows() ([]MapRow, error){
+func (q QueryBuilder) MapRows() ([]MapRow, error) {
 	return q.db.QueryMapRows(q.Sql(), q.Args()...)
+}
+
+func (q QueryBuilder) Count() (int64, error) {
+	var count int64
+	err := q.Select("select count(0)").One(&count)
+	return count, err
 }
